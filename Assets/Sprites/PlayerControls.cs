f359425e31c0ad5f6a41826d770dd77cd236bd2b
Helen See <helen.see@rutgers.edu>
2016-01-31 17:31:48 -0500

    arrow keys only movement atm

--- Assets/Sprites/PlayerControls.cs
+++ Assets/Sprites/PlayerControls.cs
@@ -1,46 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ContinouslyMove : MonoBehaviour {
-
+public class PlayerControls : MonoBehaviour {
 
     public float speed;
     public float jumpSpeed;
     public float jumpForce;
-    
+
     public Transform target;
     public CharacterController controller;
-    public float offSetx;
-    public float offSety;
 
     public bool isGrounded = true;
     private int jumpHeight = 400;
 
     private Rigidbody2D rb;
-
-    public Ground ground;
-    public Transform top_left;
-    public Transform bottom_right;
-    public LayerMask ground_layers;
-
+    private float move;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-        ground = GetComponent<Ground>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(new Vector2(-10, 0), ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(10, 0), ForceMode2D.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
-        }
-        else
-        {
-            Vector2 movement = new Vector2(2, -speed);
-            transform.Translate(movement * Time.deltaTime);
-
         }
     }
 
@@ -64,10 +58,11 @@ public class ContinouslyMove : MonoBehaviour {
         isGrounded = Physics2D.Raycast(transform.position, -Vector2.up, 0.5f);
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.CompareTag ("Star")) {
-			other.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-		}
-
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Star"))
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
 }
